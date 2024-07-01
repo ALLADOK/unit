88a6e3c9da38bc7fae23889940189aaf70735daa
ALLA <all.tarasowa808@yandex.ru>
2024-07-01 22:26:36 +0300

    Reformat test1

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,18 +1,32 @@
 package ru.netology.service;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import static org.testng.AssertJUnit.*;
 
 public class CashbackHackServiceTest {
 
+
     @Test
-    void shouldCalculateCashback(){
+    void shouldCalculateCashback() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 1_000;
+        int amount = 0;
 
         int actual = service.remain(amount);
-        int expected = 0;
+        int expected = 1_000;
 
-        assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldCalculateCashback1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+
+        int actual = service.remain(amount);
+        int expected = 500;
+
+        Assert.assertEquals(actual, expected);
     }
 }
